654ee5ea054df982a5d757b04178b8a3607c345f
Ruoqian Cheng <ruoqian.cheng@RuoqiandeMacBook-Pro.local>
2020-12-23 15:26:06 +0800

    [evacheng]feat: added header and input component

---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import './Header.css'
+
+function Header() {
+  return (
+    <header className='header'>todos</header>
+  )
+}
+
+export default Header
---
+++ src/Components/Input.js
@@ -0,0 +1,11 @@
+import './Input.css'
+
+function Input() {
+  return(
+    <section>
+      <input type="text" placeholder="What needs to be done?" className="input"></input>
+    </section>
+  )
+}
+
+export default Input
